698bed006c5cc9aa8c522491eb80fd722c3edadd
evelyn048 <ms23048@iisermohali.ac.in>
2025-10-23 17:49:32 +0530

    Finding contours

--- Preprocessing.py
+++ Preprocessing.py
@@ -21,10 +21,24 @@ def detect_shapes(image_path, target_size=(800, 800)):
     gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
 
     # --- Preprocessing ---
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     _, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
     kernel = np.ones((3, 3), np.uint8)
     clean = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
 
     edges = cv2.Canny(clean, 100, 200)
     #combined = cv2.bitwise_or(clean, edges)
+    
+    # --- Find contours ---
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    shape_counts = {
+        "Triangle": 0,
+        "Square": 0,
+        "Rectangle": 0,
+        "Quadrilateral": 0,
+        "Pentagon": 0,
+        "Hexagon": 0,
+        "Circle": 0,
+        "Polygon": 0
+    }
+
