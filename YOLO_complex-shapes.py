9ecdf9e5f0302d1d510a0420bb38c1886234c793
DivyanshiSharma050 <ms23050@iisermohali.ac.in>
2025-10-26 18:45:32 +0530

    Training model in 50 epochs

--- YOLO_complex-shapes.py
+++ YOLO_complex-shapes.py
@@ -13,4 +13,14 @@ rf = Roboflow(api_key="QQoeAj8Mx9NUigim0MXR")
 project = rf.workspace("divyanshi-sctp1").project("fujiko-5hpzt")
 version = project.version(3)
 dataset = version.download("yolov8")
-                
+
+
+model = YOLO("yolov8n.pt")
+
+model.train(
+    data=r"E:\DS project\YOLO_DORAEMON_NEW\Fujiko-3\data.yaml",
+    epochs=50,
+    imgsz=640,
+    batch=16,
+    device='cpu'  
+)            
