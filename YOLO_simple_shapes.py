a1d6e61409eef19a2dcb726689ed376131b0630d
DivyanshiSharma050 <ms23050@iisermohali.ac.in>
2025-10-26 17:33:48 +0530

    reverting a commit

--- YOLO_simple_shapes.py
+++
@@ -1,8 +0,0 @@
-import torch
-from ultralytics import YOLO
-
-print("Torch version:", torch.__version__)
-print("CUDA available:", torch.cuda.is_available())
-
-model = YOLO("yolov8n.pt")
-print("YOLOv8 loaded successfully")
