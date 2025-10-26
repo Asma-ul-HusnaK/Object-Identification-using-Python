3defe61af867b4fad67dafa84847c33817ae1d7e
Asma-ul-HusnaK <ms23205@iisermohali.ac.in>
2025-10-26 18:58:36 +0530

    Testing the model

--- YOLO_complex-shapes.py
+++ YOLO_complex-shapes.py
@@ -1,5 +1,11 @@
 import torch
 from ultralytics import YOLO
+import os
+import random
+from PIL import Image as PILImage
+from IPython.display import display
+import numpy as np
+import cv2
 
 print("Torch version:", torch.__version__)
 print("CUDA available:", torch.cuda.is_available())
@@ -23,4 +29,40 @@ model.train(
     imgsz=640,
     batch=16,
     device='cpu'  
-)            
+)
+
+
+#Load the trained model
+weights_path = r"E:\DS project\YOLO_DORAEMON_NEW\runs\detect\train7\weights\best.pt"
+model = YOLO(weights_path)
+
+#Path to test images
+test_dir = r"E:\DS project\YOLO_DORAEMON_NEW\Fujiko-3\test\images"
+
+#Pick a random image
+all_images = [f for f in os.listdir(test_dir) if f.lower().endswith(('.jpg', '.png', '.jpeg'))]
+random_image = random.choice(all_images)
+image_path = os.path.join(test_dir, random_image)
+print(f"Running inference on: {random_image}")
+
+#Run prediction
+results = model.predict(source=image_path, conf=0.25, save=False)
+
+#Get the original image (RGB) from YOLO results
+orig_img_rgb = results[0].orig_img.copy()  # NumPy array in RGB
+
+# Overlay bounding boxes and class labels
+annotated_img = results[0].plot()  # returns RGB array (but may be interpreted as BGR)
+
+#BGR to RGB to fix color inversion
+rgb_img = cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)
+
+#Convert to PIL Image for Jupyter display
+display_img = PILImage.fromarray(rgb_img)
+
+# Image display
+display(display_img)
+
+
+
+            
