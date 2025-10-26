ba47bccd37d78ea33028d0fb1096b55526de28e0
DivyanshiSharma050 <ms23050@iisermohali.ac.in>
2025-10-26 18:43:34 +0530

    Data set - Doraemon from Roboflow

--- YOLO_complex-shapes.py
+++ YOLO_complex-shapes.py
@@ -6,3 +6,11 @@ print("CUDA available:", torch.cuda.is_available())
 
 model = YOLO("yolov8n.pt")
 print("YOLOv8 loaded successfully")
+
+
+from roboflow import Roboflow
+rf = Roboflow(api_key="QQoeAj8Mx9NUigim0MXR")
+project = rf.workspace("divyanshi-sctp1").project("fujiko-5hpzt")
+version = project.version(3)
+dataset = version.download("yolov8")
+                
